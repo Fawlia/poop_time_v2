02510c0a275ffb0be3f5d128a6d85a34793fa085
Fawlia <yohann.t@codeur.online>
2017-12-19 13:58:02 +0100

    First Commit

---
+++ index.php
@@ -0,0 +1,72 @@
+<?php
+require "./vendor/autoload.php";
+
+$router = new AltoRouter();
+
+$router->setBasePath('PT/');
+
+$loader = new Twig_Loader_Filesystem('views');
+
+
+$twig = new Twig_Environment($loader, array(
+    'cache' => false,
+	'debug' => true
+));
+
+// map homepage
+
+$router->map( 'GET', '/home', function() {
+	
+	global $twig;
+	echo $twig->render('index.html.twig', array('name' => 'Fabien'));
+	
+});
+
+
+$router->map( 'GET', '/form', function() {
+	
+	$a = ['one' => 1, 'two' => 2, 'three' => 3, 'four' => 4];
+	global $twig;
+	echo $twig->render('form.html.twig', array('data' => $a));
+	
+});
+
+
+$router->map( 'GET', '/test', function() {
+    echo "Bonjour";
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$match = $router->match();
+
+// call closure or throw 404 status
+if( $match && is_callable( $match['target'] ) ) {
+	call_user_func_array( $match['target'], $match['params'] ); 
+} else {
+	// no route was matched
+	header( $_SERVER["SERVER_PROTOCOL"] . ' 404 Not Found');
+}
+
+
+
+?>
