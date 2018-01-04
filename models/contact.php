b6149110ec69d32e22f3d14e4494381e15d708c2
Yohann <yohann.t@codeur.online>
2018-01-04 13:34:58 +0100

    formulaire contact OK

--- models/contact.php
+++ models/contact.php
@@ -1,36 +1,25 @@
 <?php
 
-$pseudo= $ville = $msg = $email = "";
-$pseudoError = $villeError = $msgError = $emailError = "";
+$error = [];
+$error['pseudo'] = $error['ville'] = $error['email'] = $error['msg'] = "";
 
 
 if (isset($_POST["pseudo"])){
 	
 	$pseudo = $_POST["pseudo"];
 	if(empty($_POST["pseudo"]) || strlen($_POST["pseudo"]) < 3 )
-			{
-			echo "Votre pseudo est trop court";
-			$prenomError = true;
-			}
-	else{
-	echo $pseudo;
-	}
-		
-		
+		{
+			$error['pseudo'] = true;
+		}		
 }
 
 if (isset($_POST["ville"])){
 	
 	$ville = $_POST["ville"];
 	if(empty($_POST["ville"]) || strlen($_POST["ville"]) < 3 )
-			{
-			echo "Veuillez rentrer un nom de Ville valide";
-			$villeError = true;
-			}
-	else{
-	echo $ville;
-	}
-		
+		{
+			$error['ville'] = true;
+		}
 }
 
 
@@ -39,14 +28,9 @@ if (isset($_POST["email"])){
 	$email = $_POST["email"];
 	
 	if(empty($_POST["email"]) || !preg_match('#^[\w.-]+@[\w.-]+\.[a-z]{2,6}$#i', $email))
-			{
-			echo "Veuillez rentrer un email valide";
-			$emailError = true;
-			}
-	else{
-	echo $email;
-	}
-		
+		{
+			$error['email'] = true;
+		}
 }
 
 if (isset($_POST["msg"])){
@@ -54,28 +38,20 @@ if (isset($_POST["msg"])){
 	$msg = $_POST["msg"];
 	
 	if(empty($_POST["msg"]) || strlen($_POST["msg"]) < 16 )
-			{
-			echo "Veuillez rentrer un message plus long valide";
-			$msgError = true;
-			}
-	else {
-	echo $msg;
-	}
-		
+		{
+			$error['msg'] = true;
+		}		
 }
 
-if ($pseudoError == true $villeError == true || $msgError == true || $emailError == true)
 
-{
-	
-	echo "IL Y A QUELQUE CHOSE DE POURRI DANS CE FORMULAIRE";
-	
-}
-
-else {
+if (empty($error)){
 	$message = " Pseudo ".$pseudo." Ville ".$ville." E-mail ".$email." Message : ".$msg;
-
+	
 	mail("yohann.thorez@gmail.com", "Envoi du formulaire", $message);
 }
+	
+	echo json_encode($error);
+
+
 
 ?>
